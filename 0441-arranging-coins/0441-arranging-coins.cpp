b32344916256876a3ec12360e6648cfaa4cc3163
Mayuri Mane <mayurimane936@gmail.com>
2025-05-02 23:08:10 +0530

    Time: 7 ms (12.21%), Space: 8.9 MB (35.97%) - LeetHub

---
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        int ans = 0;
+        for(int i=1; i<=n; i++) {
+            n = n-i;
+            if(n>=0) {
+                ans++;
+            }else {
+            return ans;
+            }
+        }
+        return ans;
+    }
+};
