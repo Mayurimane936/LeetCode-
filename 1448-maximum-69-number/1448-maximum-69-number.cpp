ea7e1e2d2ae04f6691f9e61eb8e8c1f18d21d47d
Mayuri Mane <mayurimane936@gmail.com>
2025-05-08 23:40:56 +0530

    Time: 0 ms (100%), Space: 8.1 MB (11.4%) - LeetHub

---
+++ 1448-maximum-69-number/1448-maximum-69-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maximum69Number (int num) {
+        string ans = to_string(num);
+        int n = ans.length();
+        for(int i=0; i<n; i++) {
+            if(ans[i]=='6') {
+                ans[i]= '9';
+                break;
+            }
+        }
+        cout<< ans;
+        return stoi(ans);
+      
+    }
+};
