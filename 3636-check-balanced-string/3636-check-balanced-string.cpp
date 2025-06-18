048bcc2c9aba8a7b19343a84069639c8465462b9
Mayuri Mane <mayurimane936@gmail.com>
2025-06-18 21:19:13 +0530

    Time: 1 ms (17.4%), Space: 8.5 MB (33.54%) - LeetHub

---
+++ 3636-check-balanced-string/3636-check-balanced-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isBalanced(string num) {
+        int sum1 = 0;
+        int sum2 = 0;
+        int n = num.length();
+        for(int i=0; i<n; i++) {
+            if(i%2==0) {
+                sum1+=num[i]-'0';
+            }else {
+                sum2+=num[i]-'0';
+            }
+        }
+        return sum1 == sum2;
+    }
+};
