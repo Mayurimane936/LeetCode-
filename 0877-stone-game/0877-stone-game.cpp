8098a5896a878895653dd1ab87ba4834caefe47c
Mayuri Mane <mayurimane936@gmail.com>
2026-06-17 00:00:57 +0530

    Time: 0 ms (100%), Space: 10.4 MB (65%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,35 +1,6 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
-        // int n = piles.size();
-        // int aliceCnt = 0;
-        // int bobCnt = 0;
-        // int i = 0; int j = n-1;
-        // bool isAliceTurn = true;
-        // while(i<n && j>=0) {
-        //     if(isAliceTurn){
-        //        if(piles[i]>piles[j]){
-        //             aliceCnt += piles[i];
-        //             piles[i] = -1;
-        //             i++;
-        //        }else{
-        //             aliceCnt += piles[j];
-        //             piles[j] = -1;
-        //             j--;
-        //        }
-        //     }else {
-        //         if(piles[i]>piles[j]){
-        //             bobCnt += piles[i];
-        //             piles[i] = -1;
-        //             i++;
-        //        }else{
-        //             bobCnt += piles[j];
-        //             piles[j] = -1;
-        //             j--;
-        //        }
-        //     }
-        // }
-        // return aliceCnt>bobCnt;
         return true;
     }
 };
